60ddebf87cc723905a6d62f14b5e6f9d516ced6c
Lennox-Gutierrez <cheruiyotlinnox@gmail.com>
2024-07-10 14:28:05 +0300

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -75,7 +75,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6
 module.exports = {
 
   //==============================[COMPULSORY]=====================================
-  sessionName: process.env.SESSION_ID || '08_07_2024_SIGMA_MD_hp5zwpmZ_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
+  sessionName: process.env.SESSION_ID || '10_07_2024_SIGMA_MD_MmpBFuBB_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
   botname: process.env.BOT_NAME       || 'Lennox-Machine',         // ADD YOUR BOT NAME
   ownername:  process.env.OWNER_NAME  || `Lennox-Gutierrez`, // ADD YOUR NAME
   HANDLERS: process.env.PREFIX        || '.',              // ADD YOUR BOT PREFIX
